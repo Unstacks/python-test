4228fe5ebfef52a2d45f51c952f3f45ec3f6682d
Joel Weirauch <joel@weirau.ch>
2018-10-27 20:59:10 -0400

    Add a hello <name> endpoint

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ def hello_world():
     return 'Hello, World!'
 
 
+@app.route('/hello/<string:name>')
+def hello_name(name):
+    return 'Hello, {}!'.format(name)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
