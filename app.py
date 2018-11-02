383684312518964bb9c91f5ec465c14863e67857
Joel Weirauch <joel@weirau.ch>
2018-11-01 21:09:26 -0400

    Add a version endpoint

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+VERSION="0.1"
 
 @app.route('/')
 def hello_world():
@@ -12,6 +13,10 @@ def hello_world():
 def hello_name(name):
     return 'Hello, {}!'.format(name)
 
+
+@app.route('/version')
+def app_version():
+    return VERSION
 
 if __name__ == '__main__':
     app.run(debug=True)
